a5fa2350b3d4cc287ed0bae28a727518db2ebb58
Hamza.Mrad <hamza.mrad@esprit.tn>
2022-10-24 17:01:48 +0100

    first commit

--- src/main/java/tn/spring/spring/MsApplication.java
+++ src/main/java/tn/spring/spring/MsApplication.java
@@ -2,8 +2,11 @@ package tn.spring.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsApplication {
 
 	public static void main(String[] args) {
